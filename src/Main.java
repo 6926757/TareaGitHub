fe479e5b17657f0523ed793456898a894c6f4414
DAM <DAM@TI1-04>
2019-06-04 20:00:39 +0200

    hola

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	
+	public static void mostrarPorPantalla() {
+		
+		System.out.println("Hola mundo");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		mostrarPorPantalla();
+		
+	}
+
+}
